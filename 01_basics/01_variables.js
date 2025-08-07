d55f30aea92c85f7fc66310d7bdde3701427a867
RushikeshTaware4664 <Rushikesh.taware@syborgtech.com>
2025-08-07 07:34:39 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId/*accountId is variable*/ = 9881338313 // const mean no change value in future fix value, that means the account no is lock value.
+// Variables name is reserve memory space.
+let email = 'rushi@gmail.com' //
+var accountPassword = '1234' //not prefered, because of issue in block scope and functional scope.
+accountCity = 'Pune' //in JS its accepted without assigning variable but its not good.
+let accountState;
+console.table([accountId, email, accountPassword, accountCity, accountState]);
